65aa1609f4375db5abbca0d67416971c786b2216
Eimen Nur <nur.amuni26@gmail.com>
2018-12-24 10:57:52 +0300

    create json file

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -4,16 +4,6 @@ import Book from './Book';
 import bookinfo from '../Assets/Books'
 
 class Books extends Component {
-    // bookinfo =[{
-    //     title:'IP',
-    //     Desc:'Introduction to Internet Programming',
-    //     Publisher:'Mahfuz A.'
-    
-    // },{
-    //     title:'AI',
-    //     Desc:'Introduction to Artificial Intellignece',
-    //     Publisher:'Haile A.'
-    // }]
     state = {  }
     render() { 
         return ( 
